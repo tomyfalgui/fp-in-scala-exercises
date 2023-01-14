08868e065ccfb1042a3234fd71f9312568fc06e7
Tomy Falgui <tomyfalgui@gmail.com>
2023-01-15 01:14:40 +0800

    solve ex 2.1

--- Chap2.scala
+++ Chap2.scala
@@ -1,4 +1,21 @@
 object Chap2 {
-  def main(args: Array[String]): Unit =
-    println("Hello World")
+
+  // 2.1
+  /** Write a recursive function to get the nth
+   *  fibonacci number. Must use a local tail-recursive
+   *  function.
+   *  */
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, prevAcc: Int, acc: Int): Int =
+      if (n <= 0) prevAcc
+      else go(n-1, acc, acc + prevAcc)
+
+    go(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    assert(fib(5) == 5)
+  }
+    
 }
