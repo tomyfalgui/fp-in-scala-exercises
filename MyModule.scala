de814ec70286df3f980c091d82e4798cb52532e5
Tomy Falgui <tomyfalgui@gmail.com>
2023-01-15 11:28:09 +0800

    add 2.2 exercise for isSorted

--- MyModule.scala
+++ MyModule.scala
@@ -3,6 +3,15 @@ object MyModule {
     if (n < 0) -n
     else n
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+
+  }
+
   // 2.1
   /** Write a recursive function to get the nth fibonacci number. Must use a
     * local tail-recursive function.
@@ -16,20 +25,39 @@ object MyModule {
     go(n, 0, 1)
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  // 2.2
+  /** Implement `isSorted` which checks whether an Array[A] is sorted according
+    * to a given comparison function
+    */
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) loop(n + 1)
+      else false
+
+    loop(0)
+
   }
 
-  private def formatFactorial(n: Int) = {
-    val msg = "The factorial of %d is %d."
-    msg.format(n, factorial(n))
-
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
-    println(formatFactorial(7))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("fib", 11, fib))
+
+    assert(isSorted(Array(1, 2, 3, 4), (x: Int, y: Int) => x > y) == false)
+    assert(isSorted(Array(1, 2, 3, 4), (x: Int, y: Int) => x < y))
+    assert(
+      isSorted(
+        Array("a", "bc", "def"),
+        (x: String, y: String) => x.length < y.length
+      )
+    )
+
   }
 
 }
