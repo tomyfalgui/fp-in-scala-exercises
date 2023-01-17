4cc2841a76688edc0eb250c05736fc63b8fb9e3d
Tomy Falgui <tomyfalgui@gmail.com>
2023-01-17 20:39:02 +0800

    get tail of list

---
+++ singly-linked-list.scala
@@ -0,0 +1,29 @@
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil         => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil          => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs)  => x * product(xs)
+  }
+
+  // exercise 3.2 tail function
+  def tail[A](list: List[A]): List[A] = list match {
+    case Nil              => List()
+    case Cons(head, tail) => tail
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+}
+
+// exercise 3.1 evaluates to 3
