992e342655983d3dde44510ed78866b55228fe46
Tomy Falgui <tomyfalgui@gmail.com>
2023-01-17 22:30:24 +0800

    add foldright

--- singly-linked-list.scala
+++ singly-linked-list.scala
@@ -52,6 +52,18 @@ object List {
     case Cons(_, tail) => Cons(nh, tail)
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil         => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _)
+
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
