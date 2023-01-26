7e0a6821821dc83d1c9e79a382507e533b9ab73a
Tomy Falgui <tomyfalgui@gmail.com>
2023-01-26 20:13:03 +0800

    add 4.1

---
+++ exception-handling.scala
@@ -0,0 +1,10 @@
+def failingFn(i: Int): Int = {
+  val y: Int = throw new Exception("fail!")
+
+  try {
+    val x = 42+ 5
+    x+ y
+
+  } catch { case e: Exception => 43}
+
+}
---
+++ options.scala
@@ -0,0 +1,28 @@
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+trait Option[+A] {
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case None    => None
+      case Some(a) => Some(f(a))
+    }
+  }
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    map(f).getOrElse(None)
+  }
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None    => default
+    case Some(a) => a
+  }
+  def orElse[B >: A](ob: => Option[B]): Option[B] = {
+    map(Some(_)).getOrElse(ob)
+  }
+  def filter(f: A => Boolean): Option[A] = {
+    this match {
+      case Some(a) if f(a) => this
+      case _               => None
+    }
+  }
+}
